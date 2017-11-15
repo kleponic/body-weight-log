9807fbd0e3c3c8dac0dd3af6713fcdc2dca1cbc3
zidna <zeth.dev@gmail.com>
2017-11-15 23:43:04 +0700

    add model bodyweightlog

--- bodyweightlogs/models.py
+++ bodyweightlogs/models.py
@@ -2,5 +2,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from time import timezone
+from decimal import Decimal
 
-# Create your models here.
+class BodyWeightLog(models.Model):
+    """
+    Record body weight log
+    """
+    created = models.DateTimeField(auto_now_add=True, verbose_name='Waktu Dibuat')
+    date = models.DateTimeField(default=timezone.now, verbose_name='Tanggal')
+
+    max_weight = models.DecimalField(verbose_name='Berat Maksimum', max_digits=5, decimal_places=2, default=Decimal('0'))
+    min_weight = models.DecimalField(verbose_name='Berat Minimum', max_digits=5, decimal_places=2, default=Decimal('0'))
+
+    def __unicode__(self):
+        return self.date.strftime("%d-%m-%Y")
